aee1b5daf348623cec1bd89deb34632737a101c9
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-05-04 15:54:09 +0530

    [Khamisha]Add:Create Program with Constructors

---
+++ Constructors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_Assignment
+{
+    public class Constructors
+    {
+        public string Name;
+
+        //Non parametrized Constructor
+        // It is special method used to initialize objects //It is called when an object of class is created
+        //It is used to set initial values for fields.
+        // doesnot have return type
+
+        public Constructors()
+        {
+            Name = "khamisha";
+
+        }
+    }
+    public class Constructors1
+
+    {
+        public string model;
+        //Parametrized Constructor
+        public Constructors1(string Model)
+        {
+            model = Model;
+        }
+    }
+
+    public class Constructors2
+    {
+        public string Name1;
+        public string Name2;
+        public Constructors2(string name1 , string name2) {
+
+            Name1 = name1;
+            Name2 = name2;
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day3B_Assignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Constructors obj = new Constructors();
+            Console.WriteLine(obj.Name);
+            Constructors1 obj1 = new Constructors1("ckzncndskncl");
+             Console.WriteLine(obj1.model);
+            Constructors2 obj2 = new Constructors2("fishhhhhhh", "teeeeeeeeenaaaaaaaa");
+            Console.WriteLine(obj2.Name1);
+               Console.WriteLine(obj2.Name2);
+            
+        }
+    }
+}
