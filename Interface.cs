407b4e64f90d8c1e958233eb70147160a6de1769
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-05-04 21:49:11 +0530

    [Khamisha]Add:Create Program with Interface

---
+++ Interface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_Assignment
+{
+     interface IGames
+    {
+        void Football();
+        void Hockey();
+
+    }
+
+     interface IBalls
+    {
+        void Plastic();
+        
+    }
+    class Cricket : IGames, IBalls
+    {
+        public void Football()
+        {
+            Console.WriteLine("best game");
+
+        }
+        public void Hockey()
+        {
+            Console.WriteLine("national Game");
+        }
+
+        public void Plastic()
+        {
+            Console.WriteLine("xxn xkasmnck");
+        }
+    }
+  
+    }
+
--- Program.cs
+++ Program.cs
@@ -4,14 +4,19 @@
     {
         public static void Main(string[] args)
         {
-            Constructors obj = new Constructors();
-            Console.WriteLine(obj.Name);
-            Constructors1 obj1 = new Constructors1("ckzncndskncl");
-             Console.WriteLine(obj1.model);
-            Constructors2 obj2 = new Constructors2("fishhhhhhh", "teeeeeeeeenaaaaaaaa");
-            Console.WriteLine(obj2.Name1);
-               Console.WriteLine(obj2.Name2);
-            
+            //Constructors obj = new Constructors();
+            //Console.WriteLine(obj.Name);
+            //Constructors1 obj1 = new Constructors1("ckzncndskncl");
+            //Console.WriteLine(obj1.model);
+            //Constructors2 obj2 = new Constructors2("fishhhhhhh", "teeeeeeeeenaaaaaaaa");
+            //Console.WriteLine(obj2.Name1);
+            //Console.WriteLine(obj2.Name2);
+            Cricket obj = new Cricket();
+            obj.Plastic();
+            obj.Football();
+            obj.Hockey();
+
+
         }
     }
 }
